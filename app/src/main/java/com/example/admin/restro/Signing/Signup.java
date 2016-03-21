fac6e8e0ad1b47b5ca50108d7ecf63a292381cc4
ShraddhaParekh27 <parekhshraddha27@gmail.com>
2016-03-21 16:17:15 +0530

    email comfiguration

--- app/src/main/java/com/example/admin/restro/Signing/Signup.java
+++ app/src/main/java/com/example/admin/restro/Signing/Signup.java
@@ -13,6 +13,7 @@ import com.example.admin.restro.R;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
 import android.app.ProgressDialog;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
@@ -35,24 +36,25 @@ import java.util.Map;
 
 public class Signup extends AppCompatActivity {
 
+    private static final String REGISTER_URL = "http://restro.ey.es/signinreg.php";
+    Button done;
+    EditText username, email, pass, verpassword, phone1, address1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
-        final EditText username = (EditText)findViewById(R.id.s1);
-        final EditText email = (EditText)findViewById(R.id.s2);
-        final EditText pass = (EditText)findViewById(R.id.s3);
-        final EditText verpassword = (EditText)findViewById(R.id.s4);
-        final EditText phone = (EditText)findViewById(R.id.s5);
-        final EditText address = (EditText)findViewById(R.id.s6);
-        Button done = (Button)findViewById(R.id.done);
-
+        username = (EditText) findViewById(R.id.s1);
+        email = (EditText) findViewById(R.id.s2);
+        pass = (EditText) findViewById(R.id.s3);
+        verpassword = (EditText) findViewById(R.id.s4);
+        phone1 = (EditText) findViewById(R.id.s5);
+        address1 = (EditText) findViewById(R.id.s6);
+        done = (Button) findViewById(R.id.done);
         done.setOnClickListener(new View.OnClickListener() {
-
             @Override
-
-
             public void onClick(View v) {
+                registerUser();
                 /*
                 String subject = "Confirmation Email";
                 String message = "Thanking you for Signing up for Restro";
@@ -67,8 +69,8 @@ public class Signup extends AppCompatActivity {
                 final String password = pass.getText().toString();
                 final String verify = verpassword.getText().toString();
                 final String user = username.getText().toString();
-                final String number = phone.getText().toString();
-                final String home = address.getText().toString();
+                final String number = phone1.getText().toString();
+                final String home = address1.getText().toString();
                 if (user.length() == 0) {
                     username.setError("Enter a username");
                 }
@@ -83,10 +85,10 @@ public class Signup extends AppCompatActivity {
                     verpassword.setError("Enter a Password Above");
                 }
                 if (number.length() == 0) {
-                    phone.setError("Enter your Phone Number");
+                    phone1.setError("Enter your Phone Number");
                 }
                 if (home.length() == 0) {
-                    address.setError("Enter your Address");
+                    address1.setError("Enter your Address");
                 }
                 if (!isValidEmail(emailtext) && emailtext.length() != 0) {
                     email.setError("Invalid Email");
@@ -99,17 +101,63 @@ public class Signup extends AppCompatActivity {
                     verpassword.setError("Password do not match Bro");
                 }
                 if (!isValidNumber(number)) {
-                    phone.setError("Invalid Phone Number");
+                    phone1.setError("Invalid Phone Number");
                 }
 
                 if (isValidEmail(emailtext) && (password.length() != 0 && password.length() < 13) && (password == verify)) {
+                    register(user, emailtext, password, number, home);
+
                     Intent intent = new Intent(Signup.this, MainActivity.class);
                     startActivity(intent);
                 }
             }
+        });
+    }
 
-            });
+    private void registerUser() {
+        String name = username.getText().toString().trim().toLowerCase();
+        String username = email.getText().toString().trim().toLowerCase();
+        String password = pass.getText().toString().trim().toLowerCase();
+        String address = address1.getText().toString().trim().toLowerCase();
+        String phone = phone1.getText().toString().trim().toLowerCase();
+
+    }
+
+    private void register(String name, String username, String password, String address, String phone) {
+        class RegisterUser extends AsyncTask<String, Void, String> {
+            ProgressDialog loading;
+            RegisterUserClass ruc = new RegisterUserClass();
+
+
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+                loading = ProgressDialog.show(Signup.this, "Please Wait", null, true, true);
+            }
+
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+                loading.dismiss();
+                Toast.makeText(getApplicationContext(), s, Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            protected String doInBackground(String... params) {
+
+                HashMap<String, String> data = new HashMap<String, String>();
+                data.put("name", params[0]);
+                data.put("username", params[1]);
+                data.put("password", params[2]);
+                data.put("address", params[3]);
+                data.put("phone", params[4]);
+
+                String result = ruc.sendPostRequest(REGISTER_URL, data);
+
+                return result;
+            }
         }
+    }
 
 
     private boolean isValidEmail(String emailtext) {
