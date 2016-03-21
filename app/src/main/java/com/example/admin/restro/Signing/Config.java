ee61e5bdf5d56a5db61d668e4271a41f324262b3
ShraddhaParekh27 <parekhshraddha27@gmail.com>
2016-03-21 16:47:10 +0530

    email comfiguration

---
+++ app/src/main/java/com/example/admin/restro/Signing/Config.java
@@ -0,0 +1,10 @@
+package com.example.admin.restro.Signing;
+
+/**
+ * Created by PAREKH on 20-03-2016.
+ */
+
+public class Config {
+    public static final String EMAIL ="application.restro@yahoo.in";
+    public static final String PASSWORD ="restro1234";
+}
